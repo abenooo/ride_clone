fb20bdf4573c50b9b268bec430bf02f5a49df7ba
Abenezer <abenezerkifle000@gmail.com>
2024-11-19 10:54:22 -0800

    Create referral screeen

---
+++ lib/drawer/referral_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class ReferralScreen extends StatelessWidget {
+  const ReferralScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+ return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Complain"),
+        centerTitle: false,
+        backgroundColor: Colors.green,
+        elevation: 0,
+      ),
+
+    );
+  }
+}
